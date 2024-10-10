6d974a99e1d3415d60decae5bbfb60244e903f9e
dylan <dsandalledu@gmail.com>
2024-10-09 17:03:19 -0700

    it now autohomes, then starts the data collection. still needs to be manually killed

--- src/main2.py
+++ src/main2.py
@@ -3,6 +3,7 @@
 Copyright 2015, 2016 MrTijn/Tijndagamer
 """
 
+import os
 import csv
 from mpu6050 import mpu6050
 import time
@@ -10,6 +11,10 @@ from datetime import datetime
 
 sensor = mpu6050(0x68)
 print("Accelerometer Alive")
+
+print("homing printer")
+os.system("echo G28 > ~/printer_data/comms/klippy.serial") # home printer
+time.sleep(30)
 
 # Generate a unique filename with the current date and time
 start_time = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
@@ -26,7 +31,11 @@ with open(filename, mode='w', newline='') as file:
     start_timestamp = time.perf_counter()
     desired_interval = 0.001  # 1000 Hz (1 milliseconds)
     next_capture_time = start_timestamp + desired_interval
-    
+
+    os.system("echo G91  > ~/printer_data/comms/klippy.serial") # send printer +100mm along the x axis
+    os.system("echo G1 X+100 F6000 > ~/printer_data/comms/klippy.serial") # send printer +100mm along the x axis
+    os.system("echo G90  > ~/printer_data/comms/klippy.serial") # send printer +100mm along the x axis
+
     while True:
         # Get the current time
         current_time = time.perf_counter()
@@ -53,5 +62,5 @@ with open(filename, mode='w', newline='') as file:
             # Schedule the next capture
             next_capture_time += desired_interval
 
-        # Sleep for a very short time to prevent high CPU usage
+        # time.sleep for a very short time to prevent high CPU usage
         time.sleep(0.0001)
